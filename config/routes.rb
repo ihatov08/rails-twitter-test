15cacd963048937b54dab4618a9dd9c99354a97b
tomoya <ihatov08@gmail.com>
2016-02-03 20:49:54 +0900

    第２課題ー問B実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'tweets#index'
+  resources :tweets
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
